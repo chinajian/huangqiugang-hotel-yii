a2d12a6fb597f318702f00ce317414d913665d87
chinajian <756010290@qq.com>
2018-07-19 15:50:10 +0800

    t

--- m/config/constant.php
+++ m/config/constant.php
@@ -7,4 +7,5 @@
 	define('COMMON_SITE_URL', SITE_URL.'/statics/common/');//全局	静态资源
 	define('ADMIN_SITE_URL', SITE_URL.'/statics/admin/');//后台管理 静态资源
 	define('RECEPTION_SITE_URL', SITE_URL.'/statics/reception/');//前台台管理 静态资源
+	define('SITE_ADMIN_URL', 'http://admin.ghchotel.com/');//静态资源(后台)
 ?>
